4aba9d1221e6ad9b52e895a3eef2f05f9e01477e
thatkhay <johnkingsleyegeonu>
2023-09-07 15:58:38 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return Always 0 (Success)
+ */
+init main(void)
+{
+ puts ("with proper grammer, but the outcome is a piece of art , \n")
+         retrn (0)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return Always 0 (Success)
+ */
+init main(void)
+{
+	    print("Size od a char: %d byte(s)\n" , sizeof(char))
+  print("Size od a int: %d byte(s)\n" , sizeof(int))
+  print("Size od a long int: %d byte(s)\n" , sizeof(long int))
+  print("Size od a long long int: %d byte(s)\n" , sizeof(long long in))
+ print("Size od a float int: %d byte(s)\n" , sizeof(float)
+return (0)
+}
+
+
